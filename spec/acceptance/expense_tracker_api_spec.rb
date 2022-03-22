09a82e6924c25b1e9886ce492566ace91052a6ed
arthurariza <tucaariza@gmail.com>
2022-03-22 16:08:18 -0300

    test: expense submit acceptance test

---
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,22 @@
+require 'rack/test'
+require 'json'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    it 'records submitted expenses' do
+      coffee = {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2022-02-01'
+      }
+
+      post '/expenses', JSON.generate(coffee)
+    end
+  end
+end
